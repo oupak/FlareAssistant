748bad9da1f0d7cefd852688b04ad5d6b5c4ae01
Edwin Chow <edwinchow@MacBook-Pro.hitronhub.home>
2022-07-13 12:55:54 -0700

    channel join, dc functionality added

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
+from secrets import token_hex
 import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+import radio
 
-client = discord.client()
+load_dotenv()
+
+token = os.environ.get('token')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print('Bot successfully connected')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("hello!")
+
+bot.run(token)
+
+
+
+
+
 
---
+++ radio.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+
+class Radio(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def join(self, ctx):
+        if ctx.author.voice is None:
+            await ctx.send("Please join a voice channel and then try again!")
+        else:
+            channel = ctx.message.author.voice.channel
+            if ctx.voice_client is None:
+                await channel.connect()
+            else:
+                await ctx.voice_client.move_to(channel)
+
+    @commands.command()
+    async def disconnect(self, ctx):
+        await ctx.voice_client.disconnect()
+
+    @commands.command()
+    async def add(self, ctx):
+       pass
+
+    
