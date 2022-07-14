53aae8a9501054dd936d1c1acc2a99215295da03
Edwin Chow <edwinchow@MacBook-Pro.hitronhub.home>
2022-07-13 18:26:38 -0700

    requirements.txt added

--- radio.py
+++ radio.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import youtube_dl
 import ffmpeg
+import os
 
 class Radio(commands.Cog):
     def __init__(self, bot):
@@ -25,11 +26,18 @@ class Radio(commands.Cog):
         await ctx.send("Disconnected from voice channel!")
 
     @commands.command()
-    async def play(self, ctx, url):
-       ydl_options = { 'format': 'bestaudio', 'noplaylist':   'True' }
-       ffmpeg_options = { 'before_options': '-reconnect 1   -reconnect_streamed 1 -reconnect_delay_max 5',  'options': '-vn' }
-       
-       
+    async def play(self, ctx, link : str):
+        ydl_options = { 'format': 'bestaudio', 'noplaylist':   'True' }
+        ffmpeg_options = { 'before_options': '-reconnect 1   -reconnect_streamed 1 -reconnect_delay_max 5',  'options': '-vn' }
+
+        voice_client = self.bot.voice_clients[0]
+    
+        ytdl = youtube_dl.YoutubeDL(YTDL_OPTS)
+        info = ytdl.extract_info(url, download=False)
+
+        asrc = discord.FFmpegOpusAudio(info['formats'][0]['url'], before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
+
+        voice_client.play(asrc)
 
 def setup(bot):
     bot.add_cog(Radio(bot))
